1c834fadcf68aeafe403c74d8cc409df7e62136d
Sara <s.guillenm.2022@alumnos.urjc.es>
2026-04-15 10:22:33 +0200

    Fix suma method implementation in Calculadora class

--- src/main/java/es/codeurjc/test/ejem/Calculadora.java
+++ src/main/java/es/codeurjc/test/ejem/Calculadora.java
@@ -3,10 +3,10 @@ package es.codeurjc.test.ejem;
 public class Calculadora {
 
 	public double suma(double op1, double op2) {
-		return op1 + op2;
+		return op1 * op2;
 	}
 
 	public double resta(double op1, double op2) {
-		return op1 + op2;
+		return op1 - op2;
 	}
 }
